4f45f8c6ea1fd0a43b0b1a4b89c4a194815e3dab
André Taiar <taiar@guestxor.(none)>
2010-05-14 10:45:07 -0300

    primeiro empasse

--- src/main.c
+++ src/main.c
@@ -1,22 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "grafo.h"
+#include "lista.h"
 
 int main(int argc, char **argv) {
-	if(argc < 2)
-	{
-		printf("Você deve fornecer o nome do arquivo de entrada como parâmetro de execução do programa");
+	if (argc < 2) {
+		printf(
+				"Você deve fornecer o nome do arquivo de entrada como parâmetro de execução do programa");
 		return EXIT_FAILURE;
 	}
 
 	FILE *entrada;
-	char leituras[5];
+	char leituras[5], leitura2[5];
+
+	int nElementos;
+	int i, j, k;
+	int reflexiva, irreflexiva, simetrica, assimetrica, antiSimetrica,
+			transitiva, equivalencia, ordemParcial;
+	int *elementos;
+	grafo relacoes;
+
 	entrada = fopen(argv[1], "r");
 
+	// lê o número de elementos do conjunto
+	fscanf(entrada, "%s", leituras);
+	nElementos = atoi(leituras);
+
+	// aloca o vetor de elementos
+	elementos = malloc(sizeof(int) * nElementos);
+
+	// inicia o grafo das relações
+	grafoInicia(nElementos, &relacoes);
+
+	// adiciona elementos do conjunto no vetor de conjuntos
+	for(i=0;i<nElementos;i+=1)
+	{
+		fscanf(entrada, "%s", leituras);
+		elementos[i] = atoi(leituras);
+	}
+
+	// adiciona relações dos elementos no grafo de relações
 	while(!feof(entrada))
 	{
-		fscanf(entrada, "%s", buff);
-		printf("%s\n", buff);
+		fscanf(entrada, "%s", leituras);
+		fscanf(entrada, "%s", leitura2);
+		grafoInsereAresta(atoi(leituras), atoi(leitura2), 1, &relacoes);
 	}
+
+	fclose(entrada);
+
+	grafoImprime(&relacoes);
+
+	// libera meméria
+	free(elementos);
 
 	return EXIT_SUCCESS;
 }
