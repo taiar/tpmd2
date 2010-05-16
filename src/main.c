64bbc2a4dccc65bd95e8f999c63618dc8cf05915
Andre Taiar <taiar@hotmail.com>
2010-05-16 20:27:01 -0300

    resolvido problema de indexacao de elementos. talvez colocar pesquisa com hash

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include "grafo.h"
 #include "lista.h"
 
+int encontraElemento(int*, int, int);
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		printf(
@@ -11,9 +13,9 @@ int main(int argc, char **argv) {
 	}
 
 	FILE *entrada;
-	char leituras[5], leitura2[5];
+	char leituras[5];
 
-	int nElementos, maiorElem = 0;
+	int nElementos, e1, e2;
 	int i, j, k;
 	int reflexiva, irreflexiva, simetrica, assimetrica, antiSimetrica,
 			transitiva, equivalencia, ordemParcial;
@@ -30,22 +32,21 @@ int main(int argc, char **argv) {
 	elementos = malloc(sizeof(int) * nElementos);
 
 	// adiciona elementos do conjunto no vetor de conjuntos
-	for(i=0;i<nElementos;i+=1)
-	{
+	for (i = 0; i < nElementos; i += 1) {
 		fscanf(entrada, "%s", leituras);
 		elementos[i] = atoi(leituras);
-		if(elementos[i] > maiorElem) maiorElem = elementos[i];
 	}
 
 	// inicia o grafo das relações
-	grafoInicia((maiorElem+1), &relacoes);
+	grafoInicia(nElementos, &relacoes);
 
 	// adiciona relações dos elementos no grafo de relações
-	while(!feof(entrada))
-	{
+	while (!feof(entrada)) {
 		fscanf(entrada, "%s", leituras);
-		fscanf(entrada, "%s", leitura2);
-		grafoInsereAresta(atoi(leituras), atoi(leitura2), 1, &relacoes);
+		e1 = atoi(leituras);
+		fscanf(entrada, "%s", leituras);
+		e2 = atoi(leituras);
+		grafoInsereAresta(encontraElemento(elementos, nElementos, e1), encontraElemento(elementos, nElementos, e2), 1, &relacoes);
 	}
 
 	fclose(entrada);
@@ -56,4 +57,12 @@ int main(int argc, char **argv) {
 	free(elementos);
 
 	return EXIT_SUCCESS;
+}
+
+int encontraElemento(int *v, int tam, int elem) {
+	int i;
+	for (i = 0; i < tam; i += 1)
+		if(v[i] == elem) return i;
+	return -1;
+
 }
